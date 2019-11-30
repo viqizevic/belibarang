1848ccc02a764e03cd0e916ec8dfd602fc4d1ee2
Vicky Tanzil <vicky.tanzil@gmail.com>
2019-11-30 20:06:47 +0100

    Use provider for products

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
+import 'package:belibarang/models/products.dart';
 import 'package:belibarang/screens/products_overview_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
       ),
-      home: ProductsOverviewScreen(),
     );
   }
 }
---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,25 @@
+import 'package:belibarang/models/products.dart';
+import 'package:belibarang/widgets/product_item.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final products = Provider.of<Products>(context).items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemCount: products.length,
+      itemBuilder: (ctx, i) => ProductItem(
+        products[i].title,
+        products[i].imageUrl,
+      ),
+    );
+  }
+}

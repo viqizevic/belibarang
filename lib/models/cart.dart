277ef0683f56e461360a68eef51a2f8c7f06716c
Vicky Tanzil <vicky.tanzil@gmail.com>
2019-12-03 16:50:14 +0100

    Use snackbar to confirm that item added to cart

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -68,6 +68,25 @@ class Cart with ChangeNotifier {
     notifyListeners();
   }
 
+  void removeSingleItem(String productId) {
+    if (_items.containsKey(productId)) {
+      if (1 < _items[productId].quantity) {
+        _items.update(
+          productId,
+          (existingCartItem) => CartItem(
+            id: existingCartItem.id,
+            title: existingCartItem.title,
+            price: existingCartItem.price,
+            quantity: existingCartItem.quantity - 1,
+          ),
+        );
+      } else {
+        _items.remove(productId);
+      }
+      notifyListeners();
+    }
+  }
+
   void clear() {
     _items = {};
     notifyListeners();
